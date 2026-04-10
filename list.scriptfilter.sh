a454a9a5f8cc0c384def9bb294de10cacf6ce572
Matt Henkel <guildencrantz@menagerie.cc>
2026-04-10 13:41:02 -0600

    Update to not show windows on the current space

--- list.scriptfilter.sh
+++ list.scriptfilter.sh
@@ -2,9 +2,15 @@
 
 source common.sh
 
-$_YABAI -m query --windows | $_JQ '
+CURRENT_SPACE=$($_YABAI -m query --spaces --space mouse | $_JQ .index)
+
+$_YABAI -m query --windows | $_JQ --arg CURRENT_SPACE $CURRENT_SPACE '
   {
-    "items": [ .[] |
+    "items": [ .[] | 
+      select( 
+        (.space != ($CURRENT_SPACE | tonumber)) # Summon from  all spaces except the current one
+        or ."is-minimized"                      # Always allow summoning minimized windows
+      ) |
       {
         "title": .title,
         "subtitle": .app,
